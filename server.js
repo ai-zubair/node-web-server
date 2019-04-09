afc321923d72bf9b90e074e17263fcc24316c6c8
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-10 02:04:41 +0530

    serve static assets from express web server

--- server.js
+++ server.js
@@ -2,27 +2,31 @@ const express = require('express');
 
 var app = express();
 
-app.get('/',(request,response)=>{
-    response.send({
-        name: 'Zubair',
-        age: 21,
-        subjects :['eng','math','hindi','science','sst'],
-        pass :true,
-        parents :{
-            mother : 'Rifat ara',
-            father : 'Bashir Ahmad'
-        }
-    })
-})
+app.use(express.static(__dirname+'/public'))
 
-app.get('/about',(request,response)=>{
-    response.send('<h1>This is the about route!</h1>')
-})
+// app.get('/',(request,response)=>{
+//     response.send({
+//         name: 'Zubair',
+//         age: 21,
+//         subjects :['eng','math','hindi','science','sst'],
+//         pass :true,
+//         parents :{
+//             mother : 'Rifat ara',
+//             father : 'Bashir Ahmad'
+//         }
+//     })
+// })
 
-app.get('/bad',(req,res)=>{
-    res.send({
-        error : "Ah! Snap! Couldn't fetch the resource!",
-        errCode : 'ENOTFOUND'
-    })
-})
-app.listen(3000);
+// app.get('/about',(request,response)=>{
+//     response.send('<h1>This is the about route!</h1>')
+// })
+
+// app.get('/bad',(req,res)=>{
+//     res.send({
+//         error : "Ah! Snap! Couldn't fetch the resource!",
+//         errCode : 'ENOTFOUND'
+//     })
+// })
+app.listen(3000,()=>{
+    console.log('Server fired up @ localhost:3000')
+});
