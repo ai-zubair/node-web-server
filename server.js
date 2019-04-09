83bd171fd4fea485ca9fa9fa4aea97adc352d867
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-10 01:17:22 +0530

    build the express server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/',(request,response)=>{
+    response.send({
+        name: 'Zubair',
+        age: 21,
+        subjects :['eng','math','hindi','science','sst'],
+        pass :true,
+        parents :{
+            mother : 'Rifat ara',
+            father : 'Bashir Ahmad'
+        }
+    })
+})
+
+app.get('/about',(request,response)=>{
+    response.send('<h1>This is the about route!</h1>')
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        error : "Ah! Snap! Couldn't fetch the resource!",
+        errCode : 'ENOTFOUND'
+    })
+})
+app.listen(3000);
