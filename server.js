cab5cc5cfb62ed1f77bc4afdbf4864c2e41ff2f4
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-13 10:49:19 +0530

    templating using hbs partials

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const hbs = require('hbs');
 
 var app = express();
 
-//set hbs as the view engine for express
+//configure express for supporting HBS partials
+hbs.registerPartials(__dirname+'/layout/partials')
+
+//set hbs as the view engine for express templating
 app.set('views','./layout')
 app.set('view engine','hbs');
 
+//serving static assets using express
 // app.use(express.static(__dirname+'/public'))
 
+//rendering templates using express' response.render
 app.get('/',(request,response)=>{
     response.render('homepage.hbs',{
         pageTitle : 'Homepage',
@@ -24,12 +29,15 @@ app.get('/about',(request,response)=>{
     })
 })
 
+//express' get http request route handling
 // app.get('/bad',(req,res)=>{
 //     res.send({
 //         error : "Ah! Snap! Couldn't fetch the resource!",
 //         errCode : 'ENOTFOUND'
 //     })
 // })
+
+//configuring express to listen for connections on port 3000
 app.listen(3000,()=>{
     console.log('Server fired up @ localhost:3000')
 });
