f34cca7e8356b06ab75f3c7c4af8bb9e867401ba
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-13 16:53:09 +0530

    playing with middlewares

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
+
+//request logger middleware func
+app.use((req,res,next)=>{
+    var now = Date();
+    var reqLog = `${now} : ${req.originalUrl}`;
+    console.log(reqLog);
+    fs.appendFile('server.log',reqLog+'\n',(err)=>{
+        if(err){
+            console.log('Oops! An error ocurred writing the server log!')
+        }
+    })
+    next();
+})
+
+//maintainance middleware function
+app.use((req,res)=>{
+    res.render('maintainence.hbs');
+})
 
 //configure express for supporting HBS partials
 hbs.registerPartials(__dirname+'/layout/partials')
@@ -15,8 +34,9 @@ hbs.registerHelper('getCurrentYear',(username , mood)=>`Hey ${username} why so $
 
 hbs.registerHelper('screamIt',(welcomeMsg)=>welcomeMsg.toUpperCase());
 
-//serving static assets using express
+//serving static assets using express' built in middleware
 // app.use(express.static(__dirname+'/public'))
+
 
 //rendering templates using express' response.render
 app.get('/',(request,response)=>{
